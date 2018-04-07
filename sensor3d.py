5b412a4bf992ba354e2dcf287e7dd763dcd210a7
Shehzad <shehzad_ali27@yahoo.com>
2018-04-08 01:03:08 +0800

    Force Calculation and Cluster head selection

--- sensor3d.py
+++ sensor3d.py
@@ -56,7 +56,7 @@ class Sensor:
 	nodeId=-1
 	sensor_count=0
 	location=Point(0,0,0)
-	Ec=0
+	energy=0
 	Cr=0
 	Sr=0
 	netForce=0
@@ -64,10 +64,10 @@ class Sensor:
 	cellId=-1
 	status=0	# 0 mean sleeping, 1 mean active
 	
-	def __init__(self,nodeId,location,Ec,Cr,Sr,netForce=0,nodeType=0,cellId=-1,status=0):
+	def __init__(self,nodeId,location,energy,Cr,Sr,netForce=0,nodeType=0,cellId=-1,status=0):
 		self.nodeId=nodeId
 		self.location=location
-		self.Ec=Ec
+		self.energy=energy
 		self.Cr=Cr
 		self.Sr=Sr
 		self.netForce=netForce
@@ -81,21 +81,21 @@ class Sensor:
 
 	def printInfo(self):
 		#print ("Sensor Id: %d Position: (%d,%d,%d) Remaining Charge: %d Communication Range %d Sensing Range %d", %(self.nodeId,self.x,self.y,self.z))
-		print ('Sensor Id: {0} \n Position: {1} \n Remaining Charge: {2} \n Communication Range: {3} \n Sensing Range: {4} \n Node Type: {5} \n Cell Id: {6}' \
-			.format(self.nodeId,self.location.toString(),self.Ec,self.Cr,self.Sr,self.nodeType,self.cellId))
+		print ('Sensor Id: {0} \n Position: {1} \n Remaining Energy: {2} \n Communication Range: {3} \n Sensing Range: {4} \n Node Type: {5} \n Cell Id: {6} \n NetForce: {7}' \
+			.format(self.nodeId,self.location.toString(),self.energy,self.Cr,self.Sr,self.nodeType,self.cellId,self.netForce))
 
 ##############################################################
 
 
 def distance(S1,S2):
-    d=math.sqrt(math.pow(S1.x-S2.x,2)+math.pow(S1.y-S2.y,2)+math.pow(S1.z-S2.z))
+    d=math.sqrt(math.pow(S1.x-S2.x,2)+math.pow(S1.y-S2.y,2)+math.pow(S1.z-S2.z,2))
     return d
 
-def createSensors(N,Ec,Cr,Sr):
+def createSensors(N,E,Cr,Sr):
 	contaier=[]
 	for i in range(0,N):
 		new_location=Point(rn.randrange(0,space_x,1),rn.randrange(0,space_y,1),rn.randrange(0,space_z,1))
-		new_sensor=Sensor(i,new_location,Ec,Cr,Sr,0,0,-1,0)
+		new_sensor=Sensor(i,new_location,E,Cr,Sr,0,0,-1,0)
 		contaier.append(new_sensor)
 	return contaier
 
@@ -109,7 +109,7 @@ def selectCentriod(contaier,space_x,space_y,space_z):
 	minmumDistance=min(distances)
 	i=distances.index(minmumDistance)
 	contaier[i].nodeType=1
-	return 1
+	return i
 
 def createCells(space_dimension,cRange):
 	r=0
@@ -177,33 +177,73 @@ def checkSensorLocationInCell(location,cell):
 				return 1
 	return 0
 
+def calculateForce(d,e1,e2):
+	if d==0:
+		return 0
+	else:
+		return (e1*e2)/pow(d,2)
+def calculateNetForce(container,centriod,dth):
+	for s1 in container:
+		if s1.nodeType!=1:
+			attraction_force=calculateForce(distance(s1.location,centriod.location),s1.energy,centriod.energy)
+			repulsion_force=0
+			for s2 in container:
+				d=distance(s1.location,s2.location)
+				if s1.nodeId != s2.nodeId:  
+					if d <= dth:
+						repulsion_force = repulsion_force + calculateForce(d,s1.energy,s2.energy)
+		s1.netForce=repulsion_force- attraction_force
+
+	return 1
+def selectClusterHead(container):
+	netForces=[]
+	CH={'Id':-1,'netForce':0}
+	for s in container:
+		if s.nodeType!=1:
+			# print ("nodeType :{}".format(s.nodeType))
+			s.nodeType=0
+			if CH ['Id'] == -1:
+				# print('1 :Cluster head id {}, NetForce {}'.format(s.nodeId,s.netForce))
+				CH ['Id'] =s.nodeId
+				CH ['netForce']=s.netForce
+				# print('1 :Cluster head id {}, NetForce {}'.format(CH['Id'],CH['netForce']))
+			elif CH['netForce'] > s.netForce:
+				# print('2 :Cluster head id {}, NetForce {}'.format(s.nodeId,s.netForce))
+				CH['Id']=s.nodeId
+				CH['netForce']=s.netForce
+				# print('2 :Cluster head id {}, NetForce {}'.format(CH['Id'],CH['netForce']))
+	for s in container:
+		if s.nodeId == CH['Id']:
+			s.nodeType=2
+	return CH['Id']
+
 ##############################################################
 
 sensorsContainer=[]
 cellContainer=[]
-initialElectricCharge=50 # initial value for electric charge
-cRange=20	# Communication range 
+initialEnergy=50 # initial value for electric charge
+cRange=15	# Communication range 
 sRange=6	# Sensing range 
-nSensors=1000 # Number of sesors
-space_dimension=space_x=space_y=space_z=100 # space dimension
+nSensors=100 # Number of sesors
+space_dimension=space_x=space_y=space_z=50 # space dimension
 
-sensorsContainer=createSensors(nSensors,initialElectricCharge,cRange,sRange)
-#for s in sensorsContainer:
-#	s.printInfo()
+sensorsContainer=createSensors(nSensors,initialEnergy,cRange,sRange)
 centriod_index=selectCentriod(sensorsContainer,space_x,space_y,space_z)
-# sensorsContainer[centriod_index].nodeType=1
-
 cellContainer=createCells(space_dimension,cRange)
+
 print ('Total Number of Cell Created {}'.format(Cell.cell_count))
-assignCellToSensors(cellContainer,sensorsContainer)
 print('Space Dimenssion : {} * {} * {}'.format(space_x,space_y,space_z))
-print('Total Number of Cells: {}'.format(len(cellContainer)))
+
+assignCellToSensors(cellContainer,sensorsContainer)
+calculateNetForce(sensorsContainer,sensorsContainer[centriod_index],cRange)
+selectClusterHead(sensorsContainer)
+
 for s in sensorsContainer:
 	s.printInfo()
 #f = open("cell.txt", "w")
-with open("cell.txt", "w") as f:
-	for c in cellContainer:
-		f.write(c.toString())
+#with open("cell.txt", "w") as f:
+#	for c in cellContainer:
+#		f.write(c.toString())
 #f.close()
 # for c in range(0,100):
 # 	print(checkSensorLocationInCell(Point(2,1,1),cellContainer[c]))
